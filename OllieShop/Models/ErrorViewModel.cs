3a63dac8d7b8937896da4017f87bcd24fb29e35f
Siang <q15374185@gmail.com>
2023-07-15 19:14:44 +0800

    Add project files.

---
+++ OllieShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OllieShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
