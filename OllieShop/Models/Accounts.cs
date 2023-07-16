705d5d01e46a97bbeaf13b9905f85b66a4ec42c8
Siang <q15374185@gmail.com>
2023-07-17 07:03:07 +0800

    修正用戶與帳號檢視的內容，但尚未完成

--- OllieShop/Models/Accounts.cs
+++ OllieShop/Models/Accounts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OllieShop.Models;
 
@@ -7,13 +8,17 @@ public partial class Accounts
 {
     public long ACID { get; set; }
 
+    [Display(Name = "帳號")]
     public string Account { get; set; } = null!;
 
+    [Display(Name = "密碼")]
     public string Password { get; set; } = null!;
 
+    [Display(Name = "階級")]
     public string Level { get; set; } = null!;
 
     public long? URID { get; set; }
 
+    [Display(Name = "用戶編號")]
     public virtual Users? UR { get; set; }
 }
