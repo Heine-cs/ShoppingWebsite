6ac72e61bf358a5d4dc4a60ba67f99318ef83c79
Siang <q15374185@gmail.com>
2023-07-24 22:50:19 +0800

    完成三合一用戶註冊後領取身分的頁面串接

--- OllieShop/Controllers/CustomersFrontEndPageController.cs
+++ OllieShop/Controllers/CustomersFrontEndPageController.cs
@@ -27,22 +27,23 @@ namespace OllieShop.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CRID,URID")] Customers customers,bool target)
+        public async Task<IActionResult> Create([Bind("CRID,URID")] Customers customers,bool roadSwitch)
         {
             if (ModelState.IsValid)
             {
-                if (target == true){ 
+                if (roadSwitch == true){ //roadSwitch是源自用戶初次註冊的action而來，初次註冊時能選擇身分，身分選擇分歧時就要走到不同的action
+                                         //如果選擇只需要註冊用戶身分，就會走到這條路
 
                     _context.Add(customers);
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Create));//暫時先這樣，要回到登入頁面，但還沒製作
                 }
-            }
-            else
-            {
+                else //如果選擇業者與消費者身分全都要，就會走這條路
+                {
                     _context.Add(customers);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction("Create","SellersFrontEndPage");
+                    return RedirectToAction("Create", "SellersFrontEndPage");
+                }
             }
 
             return View(customers);
--- OllieShop/Models/Sellers.cs
+++ OllieShop/Models/Sellers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace OllieShop.Models;
 
@@ -7,12 +9,25 @@ public partial class Sellers
 {
     public long SRID { get; set; }
 
+    [Display(Name ="商鋪名稱")]
+    [Required(ErrorMessage = "必填欄位")]
+    [StringLength(70, ErrorMessage = "商鋪名稱不得超過70個字")]
     public string ShopNAME { get; set; } = null!;
 
+    [Display(Name = "統一編號")]
+    [Required(ErrorMessage = "必填欄位")]
+    [RegularExpression("[0-9]{8}", ErrorMessage = "統編長度為0到9的8個數字組合，請檢查是否漏填或填錯")]
     public string TaxID { get; set; } = null!;
 
+    [Display(Name = "銀行局號")]
+    [Required(ErrorMessage = "必填欄位")]
+    [MinLength(3, ErrorMessage = "銀行局號長度僅為3碼之間的數字組合，請檢查是否漏填或填錯")]
+    [MaxLength(3, ErrorMessage = "銀行局號長度僅為3碼之間的數字組合，請檢查是否漏填或填錯")]
     public string BankCode { get; set; } = null!;
 
+    [Display(Name = "銀行帳號")]
+    [Required(ErrorMessage = "必填欄位")]
+    [MinLength(12, ErrorMessage = "銀行帳號長度落在12~14碼之間的數字組合，請檢查是否漏<br>填或填錯")]
     public string BankAccount { get; set; } = null!;
 
     public long? URID { get; set; }
