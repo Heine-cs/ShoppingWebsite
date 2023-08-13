0634729ab0b2c7ec0f53edeaca23affabc49b724
Siang <q15374185@gmail.com>
2023-08-13 23:40:25 +0800

    商城主頁製作，進度到篩選與商品顯示

--- OllieShop/Controllers/CustomerCouponsBackEndPageController.cs
+++ OllieShop/Controllers/CustomerCouponsBackEndPageController.cs
@@ -21,7 +21,9 @@ namespace OllieShop.Controllers
         // GET: CustomerCouponsBackEndPage
         public async Task<IActionResult> Index(long CRID)
         {
-            var ollieShopContext = _context.CustomerCoupons.Include(c => c.CN).Include(c => c.CR).Where(c => c.CRID == CRID);
+            var ollieShopContext = _context.CustomerCoupons.Include(c => c.CN).Include(c => c.CR)
+                                                            .Where(c => c.CRID == CRID)
+                                                            .OrderByDescending(c => c.DateAdded);
             return View(await ollieShopContext.ToListAsync());
         }
 
@@ -32,8 +34,6 @@ namespace OllieShop.Controllers
         {
             ViewData["CRID"] = CRID;
             ViewData["DateAdded"] = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
-            ViewData["CNID"] = new SelectList(_context.Coupons, "CNID", "CODE");
-            
             return View();
         }
 
@@ -42,18 +42,32 @@ namespace OllieShop.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CRCNID,DateAdded,AppliedDate,CNID,CRID")] CustomerCoupons customerCoupons)
+        public async Task<IActionResult> Create([Bind("CRCNID,DateAdded,AppliedDate,CNID,CRID,CODE")] CustomerCoupons customerCoupons,string CODE)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(customerCoupons);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CNID"] = new SelectList(_context.Coupons, "CNID", "CODE", customerCoupons.CNID);
-            ViewData["CRID"] = new SelectList(_context.Customers, "CRID", "CRID", customerCoupons.CRID);
+
+                // 从数据库中查询基于提供的 CODE 检索优惠券 ID（CNID）
+                var couponIdQuery =from s in _context.Coupons
+                                   where ((s.CODE == CODE))
+                                   select s.CNID;
+                // 检查查询是否返回了任何结果
+                if (couponIdQuery.Any()) {
+                    long CNID= couponIdQuery.FirstOrDefault();
+                    // 查询 CustomerCoupons 表以检查优惠券 ID（CNID）是否已存在，已存在就不能新增重複優惠券
+                    var CouponIdExistInCustomerCouponTableStatusQuery = from s in _context.CustomerCoupons
+                                        where ((s.CNID == CNID))
+                                        select s.CNID;
+                    if (!CouponIdExistInCustomerCouponTableStatusQuery.Any()) { 
+                        customerCoupons.CNID = CNID;
+                        _context.Add(customerCoupons);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction("Index", new {CRID = customerCoupons.CRID});
+                    }
+                }
+            ViewData["CODE"] = CODE;
+            ViewData["CRID"] = customerCoupons.CRID;
+            ViewData["DateAdded"] = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            ViewData["ErrorMessage"] = couponIdQuery.Any() == false? "輸入折價券代碼錯誤，請重新輸入":"已擁有該折價券代碼之票券";
             return View(customerCoupons);
         }
-
     }
 }
--- OllieShop/Models/Orders.cs
+++ OllieShop/Models/Orders.cs
@@ -1,36 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OllieShop.Models;
 
 public partial class Orders
 {
+    [Display(Name ="編號")]
     public long ORID { get; set; }
 
+    [Display(Name = "訂購日")]
+    [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh點:mm分:ss秒}")]
+    [DataType(DataType.DateTime)]
     public DateTime OrderDate { get; set; }
 
+    [Display(Name = "付款日")]
+    [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh點:mm分:ss秒}")]
+    [DataType(DataType.DateTime)]
     public DateTime? PaymentDate { get; set; }
 
+    [Display(Name = "出貨日")]
+    [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh點:mm分:ss秒}")]
+    [DataType(DataType.DateTime)]
     public DateTime? ShippedDate { get; set; }
 
+    [Display(Name = "送達日")]
+    [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh點:mm分:ss秒}")]
+    [DataType(DataType.DateTime)]
     public DateTime? ArrivalDate { get; set; }
 
+    [Display(Name = "已取消")]
     public bool Canceled { get; set; }
 
+    [Display(Name = "已鎖定")]
     public bool Locked { get; set; }
 
+    [Display(Name = "配送方式編號")]
     public string? SVID { get; set; }
 
+    [Display(Name = "消費者編號")]
     public long? CRID { get; set; }
 
+    [Display(Name = "地址組編號")]
     public long? ASID { get; set; }
 
+    [Display(Name = "信用卡編號")]
     public short? PCID { get; set; }
 
+    [Display(Name = "商家編號")]
     public long? SRID { get; set; }
 
+    [Display(Name = "付款方式編號")]
     public string? PMID { get; set; }
 
+    [Display(Name = "折價券編號")]
     public long? CNID { get; set; }
 
     public virtual Addresses? AS { get; set; }
