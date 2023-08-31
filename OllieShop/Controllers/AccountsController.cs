e52546d6bc36082842a21aeb7c84fa6a824c3ad6
Siang <q15374185@gmail.com>
2023-09-01 00:33:37 +0800

    修復登入畫面的驗證器

--- OllieShop/Controllers/AccountsController.cs
+++ OllieShop/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OllieShop.Models;
 
 namespace OllieShop.Controllers
@@ -21,12 +22,18 @@ namespace OllieShop.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult UserLogin(Accounts accounts)
         {
-            if(accounts == null)
+            if (ModelState.IsValid)
             {
-                return View();
-            }
-
-            return View();
-        }
+                var result = _context.Accounts.FirstOrDefault(a => a.Account == accounts.Account && a.Password == accounts.Password);
+                if (result == null)
+                {
+                    ViewData["ErrorMessage"] = "帳號或密碼錯誤";
+                    return View(accounts);
+                }
+				HttpContext.Session.SetString("User", JsonConvert.SerializeObject(result));
+				return RedirectToAction("Index", "Home");
+			}
+			return View(accounts);
+		}
     }
 }
